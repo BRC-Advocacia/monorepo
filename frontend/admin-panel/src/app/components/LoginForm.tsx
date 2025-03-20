0711a989b0d536f5e42f735590198d995db11bb2
Batissta <francinaldofilho05@gmail.com>
2025-03-20 19:26:35 -0300

    Refactor - Transformando o form de login em um componente independente

---
+++ frontend/admin-panel/src/app/components/LoginForm.tsx
@@ -0,0 +1,72 @@
+"use client";
+import axios from "axios";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
+        {
+          username,
+          password,
+        }
+      );
+
+      const timestamp = Date.now();
+
+      localStorage.setItem("access_token", response.data.access_token);
+      localStorage.setItem("token_timestamp", timestamp.toString());
+      router.push("/dashboard");
+    } catch {
+      setError("Credenciais inválidas.");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col w-[80%]">
+      <div className="mb-4">
+        <label className="block text-sm font-medium mb-2 text-gray-200">
+          Usuário
+        </label>
+        <input
+          type="text"
+          placeholder="Nome de usuário"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="bg-slate-950 w-full text-white p-2 border rounded border-gray-500 focus:outline-none focus:bg-slate-800 hover:border-indigo-800"
+          required
+        />
+      </div>
+      <div className="mb-6">
+        <label className="block text-sm font-medium mb-2 text-gray-200">
+          Senha
+        </label>
+        <input
+          type="password"
+          placeholder="Senha do usuário"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="bg-slate-950 w-full text-white p-2 border rounded border-gray-500 focus:outline-none focus:bg-slate-800  hover:border-indigo-800"
+          required
+        />
+      </div>
+      {error && <p className="text-red-500 mb-4 text-gray-400">{error}</p>}
+      <button
+        type="submit"
+        className="bg-black border border-gray-500 text-slate-200 p-2 rounded w-[40%] cursor-pointer hover:bg-black hover:text-slate-200 hover:border-indigo-800 transition duration-200"
+      >
+        Entrar
+      </button>
+    </form>
+  );
+};
+
+export default LoginForm;
