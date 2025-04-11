30d316f2fbd137991db8257bc2fb57e1fb4663e4
Batissta <francinaldofilho05@gmail.com>
2025-04-11 18:24:06 -0300

    refactor: organizing the folder 'components'

--- frontend/admin-panel/src/app/components/article/ArticlesDashboard.tsx
+++ frontend/admin-panel/src/app/components/article/ArticlesDashboard.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 
 import { ArticleCard } from "./ArticleCard";
-import ProtectedRoute from "./ProtectedRoute";
+import ProtectedRoute from "../ProtectedRoute";
 
 export interface Article {
   id: number;
