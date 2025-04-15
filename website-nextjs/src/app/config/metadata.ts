da5b5a83f5f18980ba098ea0489057484d1caffa
Batissta <francinaldofilho05@gmail.com>
2025-04-14 22:15:45 -0300

    feat: homePage metadata

---
+++ website-nextjs/src/app/config/metadata.ts
@@ -0,0 +1,7 @@
+import { Metadata } from "next";
+
+export const homePageMetadata: Metadata = {
+  title: "BRCR Advogados",
+  description: `Somos um escritório
+  de advocacia situado em Campina Grande e Esperança - PB. Com uma infraestrutura moderna e personalizada, atendemos clientes de todo o Brasil.`,
+};
