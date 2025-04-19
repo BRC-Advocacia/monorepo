268fbf24f60ad8eb7cc5df2af87e4f8f8ecbdacd
Batissta <francinaldofilho05@gmail.com>
2025-04-19 11:59:51 -0300

    feat: thrid section

--- website-nextjs/src/app/(home)/page.tsx
+++ website-nextjs/src/app/(home)/page.tsx
@@ -1,11 +1,13 @@
 import FirstSection from "./sections/FirstSection";
 import SecondSection from "./sections/SecondSection";
+import ThridSection from "./sections/ThridSection";
 
 export default async function Home() {
   return (
     <>
       <FirstSection />
       <SecondSection />
+      <ThridSection />
     </>
   );
 }
