77f1e8817646aedbc02db4852d6a35447055c2d3
Batissta <francinaldofilho05@gmail.com>
2025-04-23 20:41:13 -0300

    feat: fourth section first steps at home page

---
+++ website-nextjs/src/app/(home)/sections/FourthSection.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const FourthSection = () => {
+  return (
+    <section className="flex flex-col gap-6 px-5 py-[4rem] md:px-10 relative max-h-auto lg:max-h-screen">
+      <div>
+        <p className="fadeInDown font-1-m cor-6 text-center sm:text-start">
+          RESPONSABILIDADE E TRANSPARÊNCIA
+        </p>
+        <h2 className="cor-azul-escuro font-3-xxl text-center sm:text-start">
+          nosso time<span className="cor-p5">.</span>
+        </h2>
+      </div>
+      <div></div>
+    </section>
+  );
+};
+
+export default FourthSection;
