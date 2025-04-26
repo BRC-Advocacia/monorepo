a94c7ddf80142a6fdc93b40f0535dc7ee45297da
Batissta <francinaldofilho05@gmail.com>
2025-04-26 08:46:58 -0300

    feat: first start of fifth section at homepage

---
+++ website-nextjs/src/app/(home)/sections/FifthSection.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const FifthSection = () => {
+  return (
+    <section className="flex flex-col gap-6 px-5 py-[4rem] md:px-10 relative max-h-auto bg-slate-800">
+      <div>
+        <p className="fadeInDown font-1-m cor-4 text-center sm:text-start">
+          ATENDIMENTO EM TODO O BRASIL
+        </p>
+        <h2 className="cor-0 font-3-xxl text-center sm:text-start">
+          nossos escritórios<span className="cor-p5">.</span>
+        </h2>
+      </div>
+    </section>
+  );
+};
+
+export default FifthSection;
