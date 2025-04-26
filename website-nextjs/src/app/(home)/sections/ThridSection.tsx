1599a09dde44694d7de44088bac38674879801e0
Batissta <francinaldofilho05@gmail.com>
2025-04-26 08:46:23 -0300

    fix: responsivity issue

--- website-nextjs/src/app/(home)/sections/ThridSection.tsx
+++ website-nextjs/src/app/(home)/sections/ThridSection.tsx
@@ -4,17 +4,17 @@ import React from "react";
 
 const ThridSection = () => {
   return (
-    <section className="relative h-[90vh] lg:h-[95vh]">
+    <section className="relative h-auto">
       <Image
         priority
         alt="Imagem de background para a seção de introdução da página."
         src={"/img/home/atuacao-bg.jpeg"}
         width={1200}
         height={1200}
-        className="w-screen h-[90vh] lg:h-[95vh] blur-[4px] object-cover object-top absolute top-0 z-[-10]"
+        className="w-screen h-full blur-[4px] object-cover object-top absolute top-0 z-[-10]"
         sizes="100vh"
       />
-      <div className="absolute top-0 h-[90vh] lg:h-[95vh] left-0 right-0 bg-slate-800/90 z-[-9]" />
+      <div className="absolute top-0 h-full left-0 right-0 bg-slate-800/90 z-[-9]" />
       <div className="py-[4rem] w-full h-full px-5 md:px-10 flex flex-col top-0">
         <div className="mb-10 lg:mb-15">
           <p className="fadeInDown font-1-m cor-4 container text-center sm:text-start">
