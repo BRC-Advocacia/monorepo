7ba8be9136706dfa65cf622cd182440f97e74eba
Batissta <francinaldofilho05@gmail.com>
2025-04-30 18:33:14 -0300

    styles: ui issue fixed

--- website-nextjs/src/app/(home)/sections/SecondSection.tsx
+++ website-nextjs/src/app/(home)/sections/SecondSection.tsx
@@ -5,14 +5,14 @@ const SecondSection = () => {
     <section className="grid grid-cols-1 lg:grid-cols-[1fr_2fr] gap-6 px-5 md:px-10 relative max-h-auto">
       <div className="pt-[4rem]">
         <div className="mb-5 lg:mb-10">
-          <p className="fadeInDown font-1-m cor-6 text-center sm:text-start">
+          <p className="fadeInDown font-1-m cor-6 text-center sm:text-start ml-0 sm:ml-1">
             RESPONSABILIDADE E TRANSPARÊNCIA
           </p>
           <h2 className="cor-azul-escuro font-3-xxl text-center sm:text-start">
             quem somos<span className="cor-p5">.</span>
           </h2>
         </div>
-        <p className="font-1-s cor-azul-escuro text-center sm:text-start">
+        <p className="font-1-s cor-azul-escuro text-center sm:text-start ml-0 sm:ml-1">
           Um escritório que alinha tradição e inovação para fornecer soluções
           jurídicas excepcionais. Fundado com o compromisso de oferecer serviços
           jurídicos de excelência, nosso escritório tem uma orgulhosa história
