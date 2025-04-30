24f0d0251ce0265b69f6e2f3cca9358b3c0f6ebe
Batissta <francinaldofilho05@gmail.com>
2025-04-30 16:31:23 -0300

    styles: pallete fixed

--- website-nextjs/src/components/home/CardCarousel.tsx
+++ website-nextjs/src/components/home/CardCarousel.tsx
@@ -27,63 +27,95 @@ export type AreaProps = {
 const areasAtuacao: AreaProps[] = [
   {
     title: "Imobiliário",
-    icon: <Building2 width={18} height={18} className="cor-12 opacity-70" />,
+    icon: (
+      <Building2
+        width={18}
+        height={18}
+        className="cor-azul-escuro opacity-70"
+      />
+    ),
     description:
       "Especializados em Direito Imobiliário, oferecemos assessoria completa para compra, venda, locação e regularização de imóveis, garantindo segurança jurídica e soluções personalizadas.",
   },
   {
     title: "Empresarial",
-    icon: <Building2 width={18} height={18} className="cor-12 opacity-70" />,
+    icon: (
+      <Building2
+        width={18}
+        height={18}
+        className="cor-azul-escuro opacity-70"
+      />
+    ),
     description:
       "Advocacia especializada para atender com excelência a sua empresa atuando desde o planejamento societário até consultoria empresarial.",
   },
   {
     title: "Societário",
-    icon: <Handshake width={18} height={18} className="cor-12 opacity-70" />,
+    icon: (
+      <Handshake
+        width={18}
+        height={18}
+        className="cor-azul-escuro opacity-70"
+      />
+    ),
     description:
       "Atuamos em Direito Societário com expertise na constituição, reestruturação e dissolução de empresas, oferecendo soluções estratégicas para fusões, aquisições e governança corporativa.",
   },
   {
     title: "Trabalhista",
     icon: (
-      <BriefcaseBusiness width={18} height={18} className="cor-12 opacity-70" />
+      <BriefcaseBusiness
+        width={18}
+        height={18}
+        className="cor-azul-escuro opacity-70"
+      />
     ),
     description:
       "Promovemos equilíbrio nas relações de trabalho, com soluções legais e extrajudiciais para garantir justiça e harmonia.",
   },
   {
     title: "Tributário",
-    icon: <Landmark width={18} height={18} className="cor-12 opacity-70" />,
+    icon: (
+      <Landmark width={18} height={18} className="cor-azul-escuro opacity-70" />
+    ),
     description:
       "Equipe especializada em direito tributário oferece soluções eficientes, estratégias inteligentes e tranquilidade para clientes em questões fiscais complexas.",
   },
   {
     title: "Civil",
-    icon: <Scale width={18} height={18} className="cor-12 opacity-70" />,
+    icon: (
+      <Scale width={18} height={18} className="cor-azul-escuro opacity-70" />
+    ),
     description:
       "Atuação especializada nas diversas relações e negócios jurídicos do direito civil, de modo consultivo, preventivo, contencioso e financeiro.",
   },
   {
     title: "Bancário",
-    icon: <Landmark width={18} height={18} className="cor-12 opacity-70" />,
+    icon: (
+      <Landmark width={18} height={18} className="cor-azul-escuro opacity-70" />
+    ),
     description:
       "Atuação especializada com experiência em tramitações de revisionais e cobranças com recuperação de créditos e financiamentos.",
   },
   {
     title: "Consumidor",
-    icon: <Landmark width={18} height={18} className="cor-12 opacity-70" />,
+    icon: (
+      <Landmark width={18} height={18} className="cor-azul-escuro opacity-70" />
+    ),
     description:
       "Proporcionamos assistência abrangente em garantias, contratos e controvérsias, assegurando a proteção dos direitos dos consumidores e resoluções justas.",
   },
   {
     title: "Família",
-    icon: <Scale width={18} height={18} className="cor-12 opacity-70" />,
+    icon: (
+      <Scale width={18} height={18} className="cor-azul-escuro opacity-70" />
+    ),
     description:
       "Conte com advogados qualificados para proteger seu legado, nossa missão em direito de família e sucessões, divórcio.",
   },
   {
     title: "Marcário",
-    icon: <Tag width={18} height={18} className="cor-12 opacity-70" />,
+    icon: <Tag width={18} height={18} className="cor-azul-escuro opacity-70" />,
     description:
       "Registre sua marca de forma rápida, fácil e segura. Solicite um orçamento agora mesmo com nossa equipe.",
   },
@@ -114,9 +146,9 @@ export function AreaCarousel() {
               </CarouselItem>
             ))}
         </CarouselContent>
-        <div className="flex justify-center sm:justify-end gap-2 mt-6">
-          <CarouselPrevious className="static transform-none h-8 w-8 sm:h-10 sm:w-10 cursor-pointer disabled:cursor-auto" />
-          <CarouselNext className="static transform-none h-8 w-8 sm:h-10 sm:w-10 cursor-pointer disabled:cursor-auto" />
+        <div className="flex justify-center sm:justify-end gap-2 mt-8">
+          <CarouselPrevious className="static transform-none h-12 w-12 cursor-pointer disabled:cursor-auto bg-(--azul-escuro) hover:bg-(--azul-escuro)/90 hover:text-(--cor-p2) text-(--cor-p1) border-(--cor-p1)" />
+          <CarouselNext className="static transform-none h-12 w-12 cursor-pointer disabled:cursor-auto bg-(--azul-escuro) hover:bg-(--azul-escuro)/90 hover:text-(--cor-p2) text-(--cor-p1) border-(--cor-p1)" />
         </div>
       </Carousel>
     </div>
