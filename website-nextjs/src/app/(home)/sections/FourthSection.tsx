a182571c83db37820473d5e230b1b8f95af7c55c
Batissta <francinaldofilho05@gmail.com>
2025-04-30 18:33:20 -0300

    styles: ui issue fixed

--- website-nextjs/src/app/(home)/sections/FourthSection.tsx
+++ website-nextjs/src/app/(home)/sections/FourthSection.tsx
@@ -4,7 +4,7 @@ const FourthSection = () => {
   return (
     <section className="flex flex-col gap-6 px-5 md:px-10 relative max-h-auto py-[4rem]">
       <div>
-        <p className="fadeInDown font-1-m cor-6 text-center sm:text-start">
+        <p className="fadeInDown font-1-m cor-6 text-center sm:text-start ml-0 sm:ml-1">
           RESPONSABILIDADE E TRANSPARÊNCIA
         </p>
         <h2 className="cor-azul-escuro font-3-xxl text-center sm:text-start">
