ea2d8cc616af6b2d02ca4c2b113581d1802d8cac
Batissta <francinaldofilho05@gmail.com>
2025-05-01 20:24:17 -0300

    styles: background to the design pallete

--- website-nextjs/src/app/(home)/sections/FirstSection.tsx
+++ website-nextjs/src/app/(home)/sections/FirstSection.tsx
@@ -5,16 +5,15 @@ const FirstSection = () => {
   return (
     <section className="min-h-[600px] h-screen flex justify-center relative">
       <Image
-        priority
         alt="Imagem de background para a seção de introdução da página."
         src={"/img/home/background-main.jpeg"}
         width={1200}
         height={1200}
-        className="h-full w-screen object-cover object-bottom absolute bottom-0 z-[-10]"
+        className="h-full w-screen object-cover object-bottom absolute bottom-0 z-1"
         sizes="100vw"
       />
-      <div className="absolute bottom-0 h-full left-0 right-0 bg-slate-800/90 z-[-9]" />
-      <div className="flex flex-col justify-center items-center px-5 py-[4rem]">
+      <div className="absolute bottom-0 h-full left-0 right-0 bg-(--azul-escuro)/90 z-5" />
+      <div className="flex flex-col justify-center items-center px-5 py-[4rem] z-10">
         <h1 className="cor-0 font-3-xxl text-center">
           Somos um <span className="cor-p5">escritório</span>
           <br />
