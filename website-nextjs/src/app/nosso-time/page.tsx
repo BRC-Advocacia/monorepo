3652fe47f815ddc4d57aeddd6a45ff29cc9e7bfe
Batissta <francinaldofilho05@gmail.com>
2025-05-02 17:16:11 -0300

    fix: acessibility and tsx tags

--- website-nextjs/src/app/nosso-time/page.tsx
+++ website-nextjs/src/app/nosso-time/page.tsx
@@ -4,33 +4,35 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <main className="min-h-[600px] h-screen pt-[134px] pb-[4rem] px-5 md:px-10 flex flex-col relative overflow-hidden">
-      <Image
-        src={"/img/home/office-pic.jpg"}
-        alt="Imagem de fundo da seção 'nossos escritórios' do site BRC."
-        width={1200}
-        height={1200}
-        className="absolute h-full blur-[4px] w-screen top-0 left-0 object-cover -z-14"
-      />
-      <div className="absolute top-0 h-full left-0 right-0 bg-(--azul-escuro)/90 -z-10" />
-      <div className="mb-5 lg:mb-10 z-1 flex flex-col justify-center items-center mx-auto">
-        <h1 className="cor-0 font-3-xxl text-center">
-          Conheça a nossa equipe
-          <span className="cor-p5">.</span>
-        </h1>
-        <p className="text-slate-200/65 font-2-l mt-5 text-center max-w-[55ch]">
-          Advogados experientes com anos de expertise em diversas áreas do
-          direito.
-          <br />
-          Estamos comprometidos em oferecer representação jurídica excepcional,
-          com integridade e dedicação.
-        </p>
-      </div>
-      <section className="flex">
+    <>
+      <section className="min-h-[600px] h-screen py-[4rem] px-5 md:px-10 flex relative overflow-hidden">
+        <Image
+          src={"/img/home/office-pic.jpg"}
+          alt="Imagem de fundo da seção 'nossos escritórios' do site BRC."
+          width={1200}
+          height={1200}
+          className="absolute h-full blur-[4px] w-screen top-0 left-0 object-cover -z-14"
+        />
+        <div className="absolute top-0 h-full left-0 right-0 bg-(--azul-escuro)/90 -z-10" />
+        <div className="mb-5 lg:mb-10 z-1 flex flex-col justify-center items-center mx-auto">
+          <h1 className="cor-0 font-3-xxl text-center">
+            Conheça a nossa equipe
+            <span className="cor-p5">.</span>
+          </h1>
+          <p className="text-slate-200/65 font-2-l mt-5 text-center max-w-[55ch]">
+            Advogados experientes com anos de expertise em diversas áreas do
+            direito.
+            <br />
+            Estamos comprometidos em oferecer representação jurídica
+            excepcional, com integridade e dedicação.
+          </p>
+        </div>
+      </section>
+      <section className="flex flex-col">
         {partners.map((partner) => (
           <PartnerSection key={partner.name} partner={partner} />
         ))}
       </section>
-    </main>
+    </>
   );
 }
