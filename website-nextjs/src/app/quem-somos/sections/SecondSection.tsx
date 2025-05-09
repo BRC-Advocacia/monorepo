172a5943aeb9aba3d2826deae4ad645f44b2fe5c
Batissta <francinaldofilho05@gmail.com>
2025-05-09 10:59:31 -0300

    refactor: improving ui design at secondSection

--- website-nextjs/src/app/quem-somos/sections/SecondSection.tsx
+++ website-nextjs/src/app/quem-somos/sections/SecondSection.tsx
@@ -1,17 +1,23 @@
+import { Quote } from "lucide-react";
 import Image from "next/image";
 
 const SecondSection = () => {
   return (
-    <section className="px-5 md:px-10 bg-(--background) py-[4rem] cor-azul-escuro text-center sm:text-start">
-      <h2 className="font-1-xl cor-azul-escuro mb-6">
+    <section className="px-5 md:px-10 bg-(--background) py-[4rem] cor-azul-escuro text-center sm:text-start relative">
+      <Quote
+        className="absolute top-[1rem] left-5 md:left-10 right-5 sm:right-auto mx-auto cor-4 scale-x-[-1]"
+        width={48}
+        height={48}
+      />
+      <h2 className="font-3-xxl text-center sm:text-start cor-azul-escuro mb-6">
         nossa história<span className="cor-p5">.</span>
       </h2>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-6">
         <div className="flex flex-col justify-start gap-6 font-1-s">
           <p>
-            Nos últimos anos, o quadro societário - BRCR, implementou
-            estratégias para inovar a atuação jurídica. Reformulou sua prática
-            de contencioso adequando-a à necessidade do mercado por soluções
+            Nos últimos anos, o quadro societário - BRC, implementou estratégias
+            para inovar a atuação jurídica. Reformulou sua prática de
+            contencioso adequando-a à necessidade do mercado por soluções
             rápidas e extrajudiciais, a partir da implementação de gestão de
             dados e qualidade técnica.
           </p>
@@ -25,7 +31,7 @@ const SecondSection = () => {
             Seguimos com o propósito de fortalecer nossa atuação por meio de
             parcerias sólidas e uma infraestrutura cuidadosamente pensada. O
             conceito moderno e a visão de negócio permeia por completo a atuação
-            da sociedade de advogados - BRCR.
+            da sociedade de advogados - BRC.
           </p>
         </div>
         <Image
